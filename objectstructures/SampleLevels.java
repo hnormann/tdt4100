4544be41288908e7461fa9baf5bd1b989ea248c8
normann <normann@seas.harvard.edu>
2014-01-23 12:27:49 +0100

    Levels added

---
+++ objectstructures/SampleLevels.java
@@ -0,0 +1,31 @@
+package objectstructures;
+
+public class SampleLevels {
+	// You don't necessarily have to use the representation used here for the levels.
+	// These are just here for you to have some levels to try out your implementation with.
+	static 
+	String[] level1 = {"#####",
+					   "#@$.#",
+					   "#####"},
+			 level2 = {"###  ",
+					   "#.###",
+					   "#*$ #",
+					   "# @ #",
+					   "#####"},
+			 level3 = {"#######",
+					   "#.@ # #",
+					   "#$* $ #",
+					   "#   $ #",
+					   "# ..  #",
+					   "#  *  #",
+					   "#######"},
+			 level4 = {"*###########*",
+					   "#           #",
+					   "#  ... ...  #",
+					   "#  *$$ $.$  #",
+					   "# $*+$ $*$$ #",
+					   "#  *$$ $.$  #",
+					   "#  ...  .   #",
+					   "#           #",
+					   "*###########*"};
+}
